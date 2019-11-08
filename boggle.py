2d145352ae309a76b81ae0c9f376f66fa436a6f9
geminerald <zacdwarner@gmail.com>
2019-11-08 13:57:31 +0000

    fixed bug with functions and added main function, tested functionality as user

--- boggle.py
+++ boggle.py
@@ -2,64 +2,68 @@ from string import ascii_uppercase
 from random import choice
 
 
-def make_grid(width,height):
+def make_grid(width, height):
     """
-    Make boggle grid that will hold all tiles for a game
+    Creates a grid that will hold all of the tiles
+    for a boggle game
     """
-    return {(row,col): choice(ascii_uppercase) 
-    for row in range(height)    
-    for col in range(width)
-    }
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
+        for col in range(width)}
+
 
 def neighbours_of_position(coords):
     """
-    get neighbours of a grid position
+    Get neighbours of a given position
     """
     row = coords[0]
     col = coords[1]
 
-    #assign each of the neighbours 
-    #top left to top right
-
+    # Assign each of the neighbours
+    # Top-left to top-right
     top_left = (row - 1, col - 1)
-    top_center = (row -1, col)
+    top_center = (row - 1, col)
     top_right = (row - 1, col + 1)
 
-    #left to right
+    # Left to right
     left = (row, col - 1)
+    # The `(row, col)` coordinates passed to this
+    # function are situated here
     right = (row, col + 1)
 
-    #bottom left to bottom right
+    # Bottom-left to bottom-right
+    bottom_left = (row + 1, col -1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
 
-    bottom_left = (row +1, col - 1)
-    bottom_centre = (row +1, col)
-    bottom_right = (row +1 , col + 1)
-
-    return [top_left, top_center, top_right, left, right, bottom_left, bottom_centre, bottom_right]
+    return [top_left, top_center, top_right,
+            left, right,
+            bottom_left, bottom_center, bottom_right]
 
 
 def all_grid_neighbours(grid):
     """
-    get all of possible neighbours in a grid
+    Get all of the possible neighbours for each position in
+    the grid
     """
     neighbours = {}
     for position in grid:
         position_neighbours = neighbours_of_position(position)
         neighbours[position] = [p for p in position_neighbours if p in grid]
-        return neighbours
+    return neighbours
 
 
-def path_to_word(grid,path):
+def path_to_word(grid, path):
     """
-    add all letters on the path to a string
+    Add all of the letters on the path to a string
     """
     return ''.join([grid[p] for p in path])
 
 
 def search(grid, dictionary):
     """
-    search through the paths to locate the words by matching 
-    strings to words in dictionary
+    Search thrugh the paths to locate words by matching
+    strings to words in a dictionary
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
@@ -71,20 +75,32 @@ def search(grid, dictionary):
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
-
+    
     for position in grid:
         do_search([position])
     
     words = []
     for path in paths:
-        words.append(path_to_word(grid,path))
+        words.append(path_to_word(grid, path))
     return set(words)
-
 
 
 def get_dictionary(dictionary_file):
     """
-    load dictionary file
+    Load dictionary file
     """
     with open(dictionary_file) as f:
         return [w.strip().upper() for w in f]
+def main():
+    """
+    This is the function to run the project
+    """
+    grid = make_grid(3,3)
+    dictionary = get_dictionary('words.txt')
+    words = search(grid,dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+
+if __name__ == "__main__":
+    main()
